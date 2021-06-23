6323f1f18c05c31ce043978d8cd53ea8070983ae
Erik-Jan van Kesteren <erikjanvankesteren@pm.me>
2021-06-23 16:35:12 +0200

    Store basic dataset in output folder (#1)
    * Store basic dataset in output folder
    
    Changed the first script to store as sf in output folder.
    
    * Added source data to data folder
    
    * Add readme in data folder

--- 01_data_loading.R
+++ 01_data_loading.R
@@ -1,59 +1,15 @@
+# Loading data, filtering for creating the pennsylvania sf
+# Store sf in output folder
 library(tidyverse)
 library(lubridate)
 library(sf)
-library(osmenrich)
-library(gstat)
 
-gan_sf <- read_csv("https://www.globeatnight.org/2020data/GaN2020.csv") %>% 
+gan_sf <- read_csv("data/GaN2020.csv") %>% 
   st_as_sf(coords = c("Longitude", "Latitude"), crs = 4326)
 
 states <- st_read("data/us_states/cb_2018_us_state_5m.shp")
 state_geom <- states %>% filter(NAME == "Pennsylvania") %>% st_geometry() %>% st_transform(4326)
 gan_state <- gan_sf %>% st_filter(state_geom)
 
+write_rds(gan_state, "output/gan_penn.rds")
 
-gan_state <- 
-  gan_state %>% 
-  rowwise() %>% 
-  mutate(LimitingMag = na_if(LimitingMag, -9999),
-         sun_elev = st_sun_elevation(LocalDate, LocalTime, geometry))
-  
-
-gan_state %>% 
-  ggplot() + 
-  geom_sf(data = state_geom) +
-  geom_sf(aes(colour = LimitingMag)) +
-  scale_colour_gradient(low = "#DDDD88", high = "#121233", guide = FALSE) +
-  labs(colour = "Darkness") +
-  theme(legend.position = "top")
-
-
-# quick regression thingy
-res <- residuals(lm(LimitingMag ~ CloudCover + sun_elev, data = gan_state))
-gan_state[as.numeric(names(res)),"res"] <- res
-gan_state <- st_as_sf(gan_state)
-  
-
-gan_state %>% 
-  mutate(resid = res) %>% 
-  ggplot() + 
-  geom_sf(data = state_geom) +
-  geom_sf(aes(colour = resid)) +
-  labs(colour = "Darkness") +
-  scale_color_gradient2() +
-  theme(legend.position = "top")
-
-
-
-gan_state_enriched <- 
-  gan_state %>% 
-  enrich_osm(
-    name = "streets",
-    key = "highway", 
-    type = "lines",
-    r = 500, 
-    kernel = "gaussian"
-  )
-
-
-gan_state_grid <- st_make_grid(state_geom, cellsize = .1)
