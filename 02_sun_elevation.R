2f7879455af6e0d213983f5e0d9cd2dc040a9f63
Lugtig <p.lugtig@uu.nl>
2021-07-13 17:14:29 +0200

    cleaned up the file
    cleaned up file
    added Rds datafile

--- 02_sun_elevation.R
+++ 02_sun_elevation.R
@@ -38,125 +38,18 @@ gan_state$moonfraction <- getMoonIllumination(date = gan_state$Date)$fraction
 #  illuminated fraction of the moon; varies from 0.0 (new moon) to 1.0 (full moon)
 gan_state$moonaltitude <- getMoonPosition(data = sundata)$altitude
 # moon altitude above the horizon in radians
-rm(sundata)
 
-qplot(gan_state$moonfraction)
-qplot(gan_state$moonfraction,gan_state$moonaltitude) #ok, if altitude >0 use fraction as covariate
+# some chekcs (not run)
+
+#qplot(gan_state$moonfraction)
+#qplot(gan_state$moonfraction,gan_state$moonaltitude) #ok, if altitude >0 use fraction as covariate
 gan_state$moonillumination[gan_state$moonaltitude>0.0000001] <-gan_state$moonfraction
 gan_state$moonillumination[gan_state$moonaltitude<0] <-0
-qplot(gan_state$sunaltitude) # >0 means it is up. we may want to delete >-.10
+#qplot(gan_state$sunaltitude) # >0 means it is up. we may want to delete >-.10
 
-qplot(gan_state$sunazimuth)
-qplot(gan_state$sunaltitude,gan_state$sunazimuth)
-qplot(gan_state$sunaltitude,gan_state$LimitingMag)
+#qplot(gan_state$sunazimuth)
+#qplot(gan_state$sunaltitude,gan_state$sunazimuth)
+#qplot(gan_state$sunaltitude,gan_state$LimitingMag)
 # https://www.researchgate.net/publication/308906640_Flower_clocks_time_memory_and_time_forgetting/figures?lo=1&utm_source=google&utm_medium=organic
 
-
-
-################## OLD code #################################
-st_sun_elevation <- function(ldate, ltime, point) {
-  result <- .sunPosition(
-    year  = year(ldate),
-    month = month(ldate), 
-    day   = day(ldate),
-    hour  = hour(ltime),
-    min   = minute(ltime), 
-    sec   = second(ltime),
-    lat   = st_coordinates(point)[2],
-    long  = st_coordinates(point)[1]
-  )
-  
-  result$elevation
-}
-
-
-# code from https://stackoverflow.com/questions/8708048/position-of-the-sun-given-time-of-day-latitude-and-longitude
-.sunPosition <- function(year, month, day, hour=12, min=0, sec=0,
-                        lat=46.5, long=6.5) {
-  twopi <- 2 * pi
-  deg2rad <- pi / 180
-  
-  # Get day of the year, e.g. Feb 1 = 32, Mar 1 = 61 on leap years
-  month.days <- c(0,31,28,31,30,31,30,31,31,30,31,30)
-  day <- day + cumsum(month.days)[month]
-  leapdays <- year %% 4 == 0 & (year %% 400 == 0 | year %% 100 != 0) & 
-    day >= 60 & !(month==2 & day==60)
-  day[leapdays] <- day[leapdays] + 1
-  
-  # Get Julian date - 2400000
-  hour <- hour + min / 60 + sec / 3600 # hour plus fraction
-  delta <- year - 1949
-  leap <- trunc(delta / 4) # former leapyears
-  jd <- 32916.5 + delta * 365 + leap + day + hour / 24
-  
-  # The input to the Atronomer's almanach is the difference between
-  # the Julian date and JD 2451545.0 (noon, 1 January 2000)
-  time <- jd - 51545.
-  
-  # Ecliptic coordinates
-  
-  # Mean longitude
-  mnlong <- 280.460 + .9856474 * time
-  mnlong <- mnlong %% 360
-  mnlong[mnlong < 0] <- mnlong[mnlong < 0] + 360
-  
-  # Mean anomaly
-  mnanom <- 357.528 + .9856003 * time
-  mnanom <- mnanom %% 360
-  mnanom[mnanom < 0] <- mnanom[mnanom < 0] + 360
-  mnanom <- mnanom * deg2rad
-  
-  # Ecliptic longitude and obliquity of ecliptic
-  eclong <- mnlong + 1.915 * sin(mnanom) + 0.020 * sin(2 * mnanom)
-  eclong <- eclong %% 360
-  eclong[eclong < 0] <- eclong[eclong < 0] + 360
-  oblqec <- 23.439 - 0.0000004 * time
-  eclong <- eclong * deg2rad
-  oblqec <- oblqec * deg2rad
-  
-  # Celestial coordinates
-  # Right ascension and declination
-  num <- cos(oblqec) * sin(eclong)
-  den <- cos(eclong)
-  ra <- atan(num / den)
-  ra[den < 0] <- ra[den < 0] + pi
-  ra[den >= 0 & num < 0] <- ra[den >= 0 & num < 0] + twopi
-  dec <- asin(sin(oblqec) * sin(eclong))
-  
-  # Local coordinates
-  # Greenwich mean sidereal time
-  gmst <- 6.697375 + .0657098242 * time + hour
-  gmst <- gmst %% 24
-  gmst[gmst < 0] <- gmst[gmst < 0] + 24.
-  
-  # Local mean sidereal time
-  lmst <- gmst + long / 15.
-  lmst <- lmst %% 24.
-  lmst[lmst < 0] <- lmst[lmst < 0] + 24.
-  lmst <- lmst * 15. * deg2rad
-  
-  # Hour angle
-  ha <- lmst - ra
-  ha[ha < -pi] <- ha[ha < -pi] + twopi
-  ha[ha > pi] <- ha[ha > pi] - twopi
-  
-  # Latitude to radians
-  lat <- lat * deg2rad
-  
-  # Azimuth and elevation
-  el <- asin(sin(dec) * sin(lat) + cos(dec) * cos(lat) * cos(ha))
-  az <- asin(-cos(dec) * sin(ha) / cos(el))
-  
-  # For logic and names, see Spencer, J.W. 1989. Solar Energy. 42(4):353
-  cosAzPos <- (0 <= sin(dec) - sin(el) * sin(lat))
-  sinAzNeg <- (sin(az) < 0)
-  az[cosAzPos & sinAzNeg] <- az[cosAzPos & sinAzNeg] + twopi
-  az[!cosAzPos] <- pi - az[!cosAzPos]
-
-  
-  el <- el / deg2rad
-  az <- az / deg2rad
-  lat <- lat / deg2rad
-  
-  return(list(elevation=el, azimuth=az))
-} 
+write_rds(gan_state,"gan_state with sun illumination.rds")
