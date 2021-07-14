5c2c2af525177dc57f9e9f89f2079cfa911a4a51
Erik-Jan van Kesteren <erikjanvankesteren@pm.me>
2021-07-14 11:17:47 +0200

    Cleanup repo (#10)
    * Cleaned up sun elevation
    
    now properly outputs to output folder
    
    * Added .DS_Store to gitignore
    
    * updated geoenrichment script
    
    Now works well with outline in rstudio (ctrl+shift+o)
    
    * Updated renv package versions
    
    * Use correct date + time for sun moon elevation
    
    * add post processing for dataset
    
    - also make sunmoon columnnames consistent with post processing
    - prepare model script
    - renumber scripts

--- 02_sun_elevation.R
+++ 02_sun_elevation.R
@@ -1,55 +1,44 @@
-# sun and moon elevatuion
+# sun and moon elevation
 # to use to check for brightness of sun -> delete those obs
 # and for moon brightness -> use as covariate
 # 04 July 2021
-# Peter Lugtig
+# Peter Lugtig & @vankesteren
+
+
+# Libraries
+library(tidyverse)
+library(lubridate)
+library(sf)
+library(suncalc)
 
 # load the data
-source("01_data_loading.R")
+gan_penn <- read_rds("output/gan_penn.rds")
 
-##################################
-#libraries
-library(suncalc)
-library(stringr)
+# prepare dataset for input to suncalc package
+suncalc_data <- tibble(
+  date = paste(gan_penn$UTDate, gan_penn$UTTime),
+  lat  = st_coordinates(gan_penn)[,2],
+  lon  = st_coordinates(gan_penn)[,1]
+)
 
-#example NORT RUN
-#getSunlightTimes(date = Sys.Date(), lat = 50.1, lon = 1.83, tz = "CET") #example
-#getSunlightPosition(date = NULL, lat = NULL, lon = NULL,data = NULL, keep = c("altitude", "azimuth"))
-#getMoonPosition(date = NULL, lat = NULL, lon = NULL, data = NULL, keep = c("altitude", "azimuth", "distance", "parallacticAngle"))
+# add sun and moon features to the dataset
+gan_penn_sunmoon <-
+  gan_penn %>% 
+  mutate(
+    # sun azimuth in radians (direction along the horizon, measured from south to west)
+    # e.g. 0 is south and pi * 3/4 is northwest
+    sun_azimuth   = getSunlightPosition(data = suncalc_data)$azimuth,
+    # sun altitude above the horizon in radians, 
+    # e.g. 0 at the horizon and pi/2 at the zenith (straight over your head)
+    sun_altitude  = getSunlightPosition(data = suncalc_data)$altitude,
+    # illuminated fraction of the moon; varies from 0.0 (new moon) to 1.0 (full moon)
+    moon_fraction = getMoonIllumination(date = suncalc_data$date)$fraction,
+    # moon altitude above the horizon in radians
+    # e.g. 0 at the horizon and pi/2 at the zenith (straight over your head)
+    moon_altitude = getMoonPosition(data = suncalc_data)$altitude,
+    # compute moon illumination feature
+    moon_illumination = ifelse(moonaltitude > 0, moonfraction, 0)
+  )
 
-# change the geom first to separate latitude and longitude
-gan_state$geom2 <- substring(gan_state$geometry,3,nchar(gan_state$geometry)-1)
-gan_state$latitude <- as.numeric(unlist(strsplit(gan_state$geom2,","))[[1]])
-gan_state$longitude <- as.numeric(unlist(strsplit(gan_state$geom2,","))[[2]])
-gan_state$geom2 <- NULL
-gan_state$LimitingMag[gan_state$LimitingMag==-9999] <- NA
-# merge the day and hour
-gan_state$Date <- paste(gan_state$UTDate,gan_state$UTTime)
-
-# now get the sun and moon illumination
-sundata <- data.frame(date = as.Date(gan_state$Date), # need this to deal with multipke dates in package
-                   lat = gan_state$latitude, 
-                   lon = gan_state$longitude)
-gan_state$sunazimuth <-getSunlightPosition(data = sundata)$azimuth
-#  sun azimuth in radians (direction along the horizon, measured from south to west), e.g. 0 is south and Math.PI * 3/4 is northwest
-gan_state$sunaltitude <-getSunlightPosition(data = sundata)$altitude
-# sun altitude above the horizon in radians, e.g. 0 at the horizon and PI/2 at the zenith (straight over your head)
-gan_state$moonfraction <- getMoonIllumination(date = gan_state$Date)$fraction
-#  illuminated fraction of the moon; varies from 0.0 (new moon) to 1.0 (full moon)
-gan_state$moonaltitude <- getMoonPosition(data = sundata)$altitude
-# moon altitude above the horizon in radians
-
-# some chekcs (not run)
-
-#qplot(gan_state$moonfraction)
-#qplot(gan_state$moonfraction,gan_state$moonaltitude) #ok, if altitude >0 use fraction as covariate
-gan_state$moonillumination[gan_state$moonaltitude>0.0000001] <-gan_state$moonfraction
-gan_state$moonillumination[gan_state$moonaltitude<0] <-0
-#qplot(gan_state$sunaltitude) # >0 means it is up. we may want to delete >-.10
-
-#qplot(gan_state$sunazimuth)
-#qplot(gan_state$sunaltitude,gan_state$sunazimuth)
-#qplot(gan_state$sunaltitude,gan_state$LimitingMag)
-# https://www.researchgate.net/publication/308906640_Flower_clocks_time_memory_and_time_forgetting/figures?lo=1&utm_source=google&utm_medium=organic
-
-write_rds(gan_state,"gan_state with sun illumination.rds")
+# write to output folder
+write_rds(gan_penn_sunmoon, "output/gan_penn_sunmoon.rds")
---
+++ 04_postprocess.R
@@ -0,0 +1,26 @@
+# post-process data 
+# select columns, accurately code NAs, etc
+# output final dataset to output folder
+library(tidyverse)
+library(sf)
+
+# read data
+gan_sf <- read_rds("output/gan_penn_geoenrich.rds")
+
+# post process
+gan_sf_final <- 
+  gan_sf %>% 
+  mutate(
+    LimitingMag = na_if(LimitingMag, -9999),
+    CloudCover  = factor(CloudCover, 
+                         levels = c("clear", "1/4 of sky", "1/2 of sky", "over 1/2 of sky"), 
+                         ordered = TRUE),
+    Constellation = factor(Constellation)
+  ) %>% 
+  select(sky_brightness = LimitingMag, cloud_cover = CloudCover, obs_date = UTDate, 
+         obs_time = UTTime, elevation_mtr = `Elevation(m)`, sun_altitude, 
+         moon_illumination, starts_with("motorway"), starts_with("buildings"), 
+         geometry)
+
+# save data
+write_rds(gan_sf_final, "output/gan_penn_processed.rds")
