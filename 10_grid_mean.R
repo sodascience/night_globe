9b003c8f02d6bd8c53db83cb23b0466bc22255c3
agjtimmers <a.g.j.timmers@students.uu.nl>
2021-10-06 19:20:12 +0200

    Calculate means in grid boxes

--- 10_grid_mean.R
+++ 10_grid_mean.R
@@ -2,26 +2,23 @@ library(tidyverse)
 library(sf)
 library(gstat)
 
-
 # Load data
 gan_grid_means <- read_rds("output/gan_grid_geoenrich.rds")
-gan_penn <- read_rds("output/gan_penn_processed.rds")
+gan_penn_means <- read_rds("output/gan_penn_processed.rds")
 
 # Extract needed info from df to make it easier
-pnts  <- gan_penn$geometry
+pnts  <- gan_penn_means$geometry
 boxes <- gan_grid_means$geometry
 
 # Add col to df denoting in which box the observation falls
-gan_penn$wthn <- as.integer(st_within(pnts, boxes))
-try <- as.integer(st_within(pnts, boxes))
-table(try)
+gan_penn_means$wthn <- as.integer(st_within(pnts, boxes))
 
 # Now also the na's have got a wthn column, so throw those away
-no_na <- gan_penn %>% 
+no_na <- gan_penn_means %>% 
   filter(!is.na(sky_brightness))
 
 # Check in how many boxes the observations are
-unique(no_na$wthn) #183
+sort(unique(no_na$wthn)) #183
 
 # Calculate means within boxes
 means <- no_na %>%
@@ -29,7 +26,6 @@ means <- no_na %>%
   summarise(grid_mean = mean(sky_brightness, na.rm = T)) 
 
 # Get the means to their respective grid cells and put NA in all others
-
 gan_grid_means$geom2 <- st_centroid(gan_grid_means$geometry)
 gan_grid_means$wthn <- as.integer(st_within(gan_grid_means$geom2, gan_grid_means$geometry))
 gan_grid_means$grid_mean[gan_grid_means$wthn %in% means$wthn] <- means$grid_mean
@@ -38,20 +34,20 @@ gan_grid_means$grid_mean[gan_grid_means$wthn %in% means$wthn] <- means$grid_mean
 length(gan_grid_means$grid_mean[!is.na(gan_grid_means$grid_mean)]) #183!
 
 # With observations
-gan_grid %>% 
+gan_grid_means %>% 
   mutate(pred = grid_pred_cc$var1.pred, var = grid_pred_cc$var1.var) %>% 
   ggplot() + 
   geom_sf(color = NA, mapping = aes(fill = gan_grid_means$grid_mean)) + 
-  geom_sf(data = gan_penn, mapping = aes(colour = sky_brightness)) +
+  geom_sf(data = gan_penn_means, mapping = aes(colour = sky_brightness)) +
   labs(fill = "grid_mean") +
-  scale_fill_viridis_c(direction = -1, limits = c(2, 4.5)) +
+  scale_fill_viridis_c(direction = -1, limits = c(1, 7)) + #put them on the same scale
   scale_colour_viridis_c(direction = -1)
 
 # Only the averaged squares in the grid, without the observations
-gan_grid %>% 
+gan_grid_means %>% 
   mutate(pred = grid_pred_cc$var1.pred, var = grid_pred_cc$var1.var) %>% 
   ggplot() + 
   geom_sf(color = NA, mapping = aes(fill = gan_grid_means$grid_mean)) +
   labs(fill = "grid_mean") +
-  scale_fill_viridis_c(direction = -1, limits = c(2, 4.5)) +
+  scale_fill_viridis_c(direction = -1, limits = c(1, 7)) + #scale needs to be like this, otherwise some boxes stay grey
   scale_colour_viridis_c(direction = -1)
