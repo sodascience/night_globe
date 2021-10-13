9bb8d0eba35939a744b69b5e66f3a48fd05a6761
Javier Garcia-Bernardo <garcia@uva.nl>
2021-10-13 14:38:02 +0200

    Osm local (#15)
    * simpler code, local overpass api
    
    * file from local overpass, same results

--- 03_geoenrichment.R
+++ 03_geoenrichment.R
@@ -4,245 +4,72 @@ library(tidyverse)
 library(sf)
 library(osmenrich)
 
+## If you have a local instance of the overpass api installed you can uncomment the following line (using port 12345, see below in the installation)
+# osmdata::set_overpass_url("http://localhost:12345/api/interpreter")
+
+## To install the local instance, open docker and run the following
+## code, which will install the database in Volumes/apfs_part/docker_us_storage
+## It will take ~1 hour to install, but will remove the API limitations for the OSM queries
+# docker run \
+# -e OVERPASS_META=yes \
+# -e OVERPASS_MODE=init \
+# -e OVERPASS_PLANET_URL=https://download.geofabrik.de/north-america/us/pennsylvania-latest.osm.bz2 \
+# -e OVERPASS_DIFF_URL=http://download.openstreetmap.fr/replication/north-america/minute/ \
+# -e OVERPASS_RULES_LOAD=10 \
+# -v /Volumes/apfs_part/docker_us_storage:/db \
+# -p 12345:80 \
+# -i -t \
+# --name overpass_us \
+# wiktorn/overpass-api
+
 # Load data
 gan_penn <- read_rds("output/gan_penn_sunmoon.rds")
 
-# Highways ----
-# All highways receive a 1, 10 or 25 km gaussian kernel
-## Motorway ----
-# 1km
-gan_enrich <- gan_penn %>%
-  enrich_osm(
-    name = "motorway_1km",
-    key = "highway",
-    value = "motorway",
-    type = "lines",
-    kernel = "gaussian",
-    r = 1000
-  )
+## Parameters
+# Setting up possible distances
+rs <- c(1000, 10000, 25000)
+# Kernel and type of objects
+kernel <- "gaussian"
+type <- "lines"
 
-# 10km
-gan_enrich <- gan_enrich %>%
-  enrich_osm(
-    name = "motorway_10km",
-    key = "highway",
-    value = "motorway",
-    type = "lines",
-    kernel = "gaussian",
-    r = 10000
-  )
+# Mapping of keys to values
+key_to_values = list(highway = c("motorway","trunk","primary","secondary","tertiary","unclassified"),
+                    building = c(NULL))
 
-# 25km
-gan_enrich <- gan_enrich %>%
-  enrich_osm(
-    name = "motorway_25km",
-    key = "highway",
-    value = "motorway",
-    type = "lines",
-    kernel = "gaussian",
-    r = 25000
-  )
+gan_enrich <- gan_penn
 
-## Trunk ----
-# 1km
-gan_enrich <- gan_enrich %>%
-  enrich_osm(
-    name = "trunk_1km",
-    key = "highway",
-    value = "trunk",
-    type = "lines",
-    kernel = "gaussian",
-    r = 1000
-  )
+# Keep track of time (for future performance boosting)
+start.time <- Sys.time()
 
-# 10km
-gan_enrich <- gan_enrich %>%
-  enrich_osm(
-    name = "trunk_10km",
-    key = "highway",
-    value = "trunk",
-    type = "lines",
-    kernel = "gaussian",
-    r = 10000
-  )
-
-# 25km
-gan_enrich <- gan_enrich %>%
-  enrich_osm(
-    name = "trunk_25km",
-    key = "highway",
-    value = "trunk",
-    type = "lines",
-    kernel = "gaussian",
-    r = 25000
-  )
-
-## Primary ----
-# 1km
-gan_enrich <- gan_enrich %>%
-  enrich_osm(
-    name = "primary_1km",
-    key = "highway",
-    value = "primary",
-    type = "lines",
-    kernel = "gaussian",
-    r = 1000
-  )
-
-# 10km
-gan_enrich <- gan_enrich %>%
-  enrich_osm(
-    name = "primary_10km",
-    key = "highway",
-    value = "primary",
-    type = "lines",
-    kernel = "gaussian",
-    r = 10000
-  )
-
-# 25km
-gan_enrich <- gan_enrich %>%
-  enrich_osm(
-    name = "primary_25km",
-    key = "highway",
-    value = "primary",
-    type = "lines",
-    kernel = "gaussian",
-    r = 25000
-  )
-
-## Secondary ----
-# 1km
-gan_enrich <- gan_enrich %>%
-  enrich_osm(
-    name = "secondary_1km",
-    key = "highway",
-    value = "secondary",
-    type = "lines",
-    kernel = "gaussian",
-    r = 1000
-  )
-
-# 10km
-gan_enrich <- gan_enrich %>%
-  enrich_osm(
-    name = "secondary_10km",
-    key = "highway",
-    value = "secondary",
-    type = "lines",
-    kernel = "gaussian",
-    r = 10000
-  )
-
-# 25km
-gan_enrich <- gan_enrich %>%
-  enrich_osm(
-    name = "secondary_25km",
-    key = "highway",
-    value = "secondary",
-    type = "lines",
-    kernel = "gaussian",
-    r = 25000
-  )
-
-## Tertiary ----
-# 1km
-gan_enrich <- gan_enrich %>%
-  enrich_osm(
-    name = "tertiary_1km",
-    key = "highway",
-    value = "tertiary",
-    type = "lines",
-    kernel = "gaussian",
-    r = 1000
-  )
-
-# 10km
-gan_enrich <- gan_enrich %>%
-  enrich_osm(
-    name = "tertiary_10km",
-    key = "highway",
-    value = "tertiary",
-    type = "lines",
-    kernel = "gaussian",
-    r = 10000
-  )
-
-# 25km
-gan_enrich <- gan_enrich %>%
-  enrich_osm(
-    name = "tertiary_25km",
-    key = "highway",
-    value = "tertiary",
-    type = "lines",
-    kernel = "gaussian",
-    r = 25000
-  )
-
-## Unclassified ----
-# 1km
-gan_enrich <- gan_enrich %>%
-  enrich_osm(
-    name = "unclassified_1km",
-    key = "highway",
-    value = "unclassified",
-    type = "lines",
-    kernel = "gaussian",
-    r = 1000
-  )
-
-# 10km
-gan_enrich <- gan_enrich %>%
-  enrich_osm(
-    name = "unclassified_10km",
-    key = "highway",
-    value = "unclassified",
-    type = "lines",
-    kernel = "gaussian",
-    r = 10000
-  )
-
-# 25km
-gan_enrich <- gan_enrich %>%
-  enrich_osm(
-    name = "unclassified_25km",
-    key = "highway",
-    value = "unclassified",
-    type = "lines",
-    kernel = "gaussian",
-    r = 25000
-  )
-
-
-# Buildings ----
-# Buildings receive a 1, 10 or 20 km gaussian kernel
-# 1km
-gan_enrich <- gan_enrich %>%
-  enrich_osm(
-    name = "buildings_1km",
-    key = "building",
-    kernel = "gaussian",
-    r = 1000
-  )
-
-# 10km
-gan_enrich <- gan_enrich %>%
-  enrich_osm(
-    name = "buildings_10km",
-    key = "building",
-    kernel = "gaussian",
-    r = 10000
-  )
-
-# 20km
-gan_enrich <- gan_enrich %>%
-  enrich_osm(
-    name = "buildings_20km",
-    key = "building",
-    kernel = "gaussian",
-    r = 20000
-  )
+## Enrich for the different combinations (~30 min with a local instance)
+for (key in names(key_to_values)) {
+  for (value in key_to_values[[key]]) {
+    for (r in rs) {
+      # Name of column
+      if (is.null(value)) { 
+        name = paste(key,"_",r/1000,"km",sep="")
+      } else {
+        name = paste(value,"_",r/1000,"km",sep="")
+      }
+    
+      # Enrich
+      gan_enrich <- gan_enrich %>%
+        enrich_osm(
+          name = name,
+          key = key,
+          value = value,
+          type = type,
+          kernel = kernel,
+          r = r,
+          control=list(timeout = 600) #Adding some extra time for the server to respond
+        )
+    }
+  }
+}
 
 # Write output ----
 # Store as gan_penn_geoenrich.rds
 write_rds(gan_enrich, "output/gan_penn_geoenrich.rds")
+
+# Print time taken
+print(Sys.time() - start.time)
