4aad6b93c3b449fbb6710b1dd21985648dccb06f
Erik-Jan van Kesteren <erikjanvankesteren@pm.me>
2022-05-27 14:44:14 +0200

    Model with loocv (#19)
    * update and rerun model script
    
    * Delete workspace_model.RData
    
    * major update for LOOCV
    
    - simplified models script
    - added subfolders in /data directory
    - added loocv results
    - separated out internal & external validation script
    - changed plots
    
    Co-Authored-By: agjtimmers <74608219+agjtimmers@users.noreply.github.com>
    Co-Authored-By: Peter Lugtig <p.lugtig@uu.nl>
    
    Co-authored-by: agjtimmers <74608219+agjtimmers@users.noreply.github.com>
    Co-authored-by: Peter Lugtig <p.lugtig@uu.nl>

---
+++ 08_external_validation.R
@@ -0,0 +1,59 @@
+# External validation script
+# Take the predictions for the grid
+# Compare with the external skyglow metric
+library(tidyverse)
+library(sf)
+
+# load the prediction grid
+pred_sf <- read_rds("data/pred_sf.rds")
+
+# External validation: compare the predictions to the (log-)skyglow
+# visual comparison
+pred_sf %>% 
+  as_tibble() %>%
+  pivot_longer(
+    starts_with("est"), 
+    names_to = c("Covariates", "Kriging"), 
+    names_sep = "_", 
+    names_prefix = "est_", 
+    values_to = "est"
+  ) %>% 
+  mutate(Kriging = as_factor(Kriging), Covariates = as_factor(Covariates)) %>% 
+  ggplot(aes(y = log_skyglow, x = est)) +
+  geom_pointdensity() + 
+  geom_smooth(colour = "black", se = FALSE, formula = y ~ x, method = "lm") +
+  scale_colour_viridis_c(guide = "none") +
+  theme_minimal() +
+  facet_grid(rows = vars(Covariates), cols = vars(Kriging), labeller = label_both) +
+  xlim(c(-9, 2)) +
+  ylim(-1.5, 4.5) +
+  labs(x = "Estimate", y = "Log observed skyglow")
+
+ggsave("img/comparison.png", width = 8, height = 12)
+
+# comparing correlation
+as_tibble(pred_sf) %>% 
+  select(log_skyglow, starts_with("est")) %>% 
+  cor(use = "pair", method = "spearman") 
+
+cor.test(pred_sf$skyglow, pred_sf$est_Naive_No, method = "spearman")
+cor.test(pred_sf$skyglow, pred_sf$est_Naive_Yes, method = "spearman")
+cor.test(pred_sf$skyglow, pred_sf$est_Landuse_No, method = "spearman")
+cor.test(pred_sf$skyglow, pred_sf$est_Landuse_Yes, method = "spearman")
+cor.test(pred_sf$skyglow, pred_sf$est_OSM_No, method = "spearman")
+cor.test(pred_sf$skyglow, pred_sf$est_OSM_Yes, method = "spearman")
+cor.test(pred_sf$skyglow, pred_sf$est_Both_No, method = "spearman")
+cor.test(pred_sf$skyglow, pred_sf$est_Both_Yes, method = "spearman")
+
+# comparing R²
+tibble(
+  Model = 1:8, 
+  Rsq = c(summary(lm(log_skyglow ~ est_Naive_No, na.omit(pred_sf)))$r.squared,
+          summary(lm(log_skyglow ~ est_Naive_Yes, na.omit(pred_sf)))$r.squared,
+          summary(lm(log_skyglow ~ est_Landuse_No, na.omit(pred_sf)))$r.squared,
+          summary(lm(log_skyglow ~ est_Landuse_Yes, na.omit(pred_sf)))$r.squared,
+          summary(lm(log_skyglow ~ est_OSM_No, na.omit(pred_sf)))$r.squared,
+          summary(lm(log_skyglow ~ est_OSM_Yes, na.omit(pred_sf)))$r.squared,
+          summary(lm(log_skyglow ~ est_Both_No, na.omit(pred_sf)))$r.squared,
+          summary(lm(log_skyglow ~ est_Both_Yes, na.omit(pred_sf)))$r.squared)
+)
